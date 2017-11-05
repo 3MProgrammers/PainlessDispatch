7a9b4dbe8ef419165b6bfd236f70866ca4046ec3
mpare_000 <mpare_000@ASUS-X555L>
2017-11-05 15:33:12 -0500

    Add project files.

---
+++ PainlessDispatch/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace PainlessDispatch.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ PainlessDispatch/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PainlessDispatch
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //mgp
+            if (Session["SignedIn"] == null || ((bool)Session["SignedIn"] != true))
+            { 
+            Response.Redirect("http://localhost:4638/Account/Login");
+            }
+        }
+    }
+}
